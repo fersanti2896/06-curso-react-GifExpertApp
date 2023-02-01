66ba24b7eaaeba148ebe84663bbdfc6ba9aca3af
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-01 17:25:15 -0600

    Uso del hook useEffect en GifGrid.jsx

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,9 +1,15 @@
+import { useEffect } from 'react';
 import PropTypes, { func } from 'prop-types';
+
 import { getGifs } from '../helpers/getGifs';
 
-export const GifGrid = ({ category }) => {
-    getGifs(category);
 
+export const GifGrid = ({ category }) => {
+
+    useEffect(() => {
+        getGifs(category);    
+    }, []);
+    
     return (
         <>
             <h3>{ category }</h3>
