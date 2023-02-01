011671c8242b302707527756b96f77226f66566d
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-01 02:31:19 -0600

    Creando categorias en GifExpertApp.jsx

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,27 @@
-
+import { useState } from 'react';
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        <h1>GifExpertApp</h1>
-    </>
-  )
+    const [ categories, setCategories ] = useState([ 'React', 'Angular', 'Vue' ]);
+    
+    const addCategory = () => {
+        setCategories([ ...categories, 'Entity Framework']);
+    }
+
+    return (
+        <>
+            {/* Title */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gifs */}
+            <button onClick={ addCategory }>Agregar</button>
+            <ol>
+                { categories.map( cat => {
+                    return <li key={ cat }>{ cat }.</li>
+                } ) }
+            </ol>
+                {/* Gif Item */}
+        </>
+    )
 }
